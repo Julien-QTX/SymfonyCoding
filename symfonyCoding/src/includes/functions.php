b9de2140b1f568842523e27814d66c85d16dbcee
DevMehd <60779348+DevMehd@users.noreply.github.com>
2023-09-21 18:48:08 +0200

    Delete functions.php

--- symfonyCoding/src/includes/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-     function createSlug($title) {
-        $slug = preg_replace('/-+/', '-', strtr($title, ['é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e', 'à' => 'a', 'â' => 'a', 'ä' => 'a', 'ô' => 'o', 'ö' => 'o', 'û' => 'u', 'ü' => 'u', 'ç' => 'c']));
-        $slug = strtolower(preg_replace('/[^a-zA-Z0-9]/', '-', $slug));
-        $slug = preg_replace('/-+/', '-', $slug);
-        return $slug;
-    }
-?>
